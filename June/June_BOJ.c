c66956f68b515c485df178f13b7ccbd2bd84f70d
JuneKim <rlawns3244@naver.com>
2019-02-13 01:15:36 +0900

    BOJ_Practice
    백준쉬운문제품

---
+++ June/June_BOJ.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+	int x,y; 
+	scanf("%d %d", &x,&y);
+	char * days[7] = { "SUN","MON","TUE","WED","THU","FRI","SAT" };
+	/*if ()
+		printf("%s", days[? ]);*/
+	return 0;
+}
